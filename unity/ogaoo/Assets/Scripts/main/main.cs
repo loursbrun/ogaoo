fb1592c1bc70eefad48dc979e4ee6b6a0e8f80cd
loursbrun <fabienbrun@hotmail.fr>
2022-01-06 17:53:28 +0100

    Add action for buttons

--- unity/ogaoo/Assets/Scripts/main/main.cs
+++ unity/ogaoo/Assets/Scripts/main/main.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class main : MonoBehaviour
-{
+
+public class main : MonoBehaviour {
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +18,18 @@ public class main : MonoBehaviour
     {
         
     }
+
+
+    public void LoadTest1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadTest2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+
+
 }
