405098f7c4571caf004fbe490f5e7264c181f298
loursbrun <fabienbrun@hotmail.fr>
2022-01-06 17:16:07 +0100

    Add Test1 and Test2 Scenes

---
+++ unity/ogaoo/Assets/Scripts/Test1/MainTest1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainTest1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Test 1 Scene !");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ unity/ogaoo/Assets/Scripts/Test2/MainTest2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainTest2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Test 2 Scene !");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
